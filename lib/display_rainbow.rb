2da48631f7ce7ecf8bba19ec837c54c7462e8576
Justine Bricknell <justinegayton@gmail.com>
2018-06-06 17:55:23 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,5 @@
-colors = ["R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"]
+colors = ["R: red", "O: orange," "Y: yellow," "G: green," "B: blue," "I: indigo," "V: violet"]
 
-def display_rainbow (colors)
+def display_rainbow (colors [0,1,2,3,4,5,6])
   puts ""
 end
